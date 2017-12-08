8c6d7c8655b6814a37f52c7b821edc307af119dd
stan wielga <svwielga4@gmail.com>
2017-12-08 11:43:43 -0800

    adding init / dom creation

--- script.js
+++ script.js
@@ -1,13 +1,55 @@
-$(document).ready(function(){
-    console.log('testing');
+$(document).ready(function() {
+  var calc = new BodyCreator();
+  calc.init();
+});
 
-    var calc = new Calculator();
-    calc.init();
+/*************************
+ *Calculator body creator
+ * @constructor
+ */
+function BodyCreator() {
+  var self = this;
+  this.buttonMap = [
+      ['+','-','*','/'],
+      ['1','2','3','4'],
+      ['5','6','7','8'],
+      ['9','0','C','=']
+  ]
+  this.body = $(".calculatorBody");
+  this.face;
+  this.buttonRows;
 
+  this.init = function() {
+    this.addFace();
+    this.registerFace();
+    this.addButtonRows(self.buttonMap.length);
+    this.registerButtonRows();
+  };
 
-    function Calculator(){
-        this.init = function(){ console.log('initing') }
+  this.addFace = function() {
+    $("<div>", {
+      class: "face"
+    }).appendTo(self.body);
+  };
+
+  this.registerFace = function() {
+    self.face = $(".face");
+  };
+
+  this.addButtonRows = function(numRows) {
+    while (numRows > 0) {
+      $("<div></div>", {
+        class: "buttonRow"
+      }).appendTo(self.body);
+      numRows--;
     }
+  };
 
+  this.registerButtonRows = function() {
+    self.buttonRows = $(".buttonRow");
+  };
 
-})
+  this.addCalculatorButtons = function() {
+
+  };
+}
